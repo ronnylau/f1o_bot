5c01f119fbe8fdaa2877d1c0e14c10c5b1694987
ronnylau <uzimaster@web.de>
2022-09-05 00:27:50 +0200

    Cleanup
    Remove config.ini, we store the bot token in the enviroment

--- bot.py
+++ bot.py
@@ -4,10 +4,8 @@
 import logging
 import os
 
-import f1.config
-
 logger = logging.getLogger(__name__)
 logger.warning('Starting bot...')
 
 
-commands.bot.run(os.getenv('BOT_TOKEN', f1.config.CONFIG['BOT']['TOKEN']))
+commands.bot.run(os.getenv('BOT_TOKEN'))
